4dfc6603413f391814b732ed3e0958bb82ec1196
gcaanderson <164887088+gcaanderson@users.noreply.github.com>
2024-04-24 09:45:09 -0500

    Update test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,6 +1,7 @@
 import pytest
 # Add necessary imports
 from train_model.py import train, test, model, model_dir, model_path
+from main.py import data_processed
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_datasets_file_type(train, test):
@@ -9,15 +10,23 @@ def test_datasets_file_type(train, test):
     """
     assert train.endswith(".csv")
     assert test.endswith(".csv")
+    
     pass
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model_encoder_files():
     """
     # Check if the model and encoder have been saved successfully. 
     """
-    # Your code here
+    # Check the existence of the files
+    assert os.path.exists("model\encoder.pkl"), f"Encoder pickle file does not exist"
+    assert os.path.exists("model\model.pkl"), f"Model pickle file does not exist"
+
+    # Check if the files are empty
+    assert os.stat("model\encoder.pkl").st_size > 0, f"Encoder pickle file is empty"
+    assert os.stat("model\model.pkl").st_size > 0, f"Model pickle file is empty"
+
     pass
 
 
